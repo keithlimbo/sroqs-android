327fa9ae902e6eaf4eb93e4c06fdb7f97c9fb4b0
Keith <limbokeith71@gmail.com>
2020-10-30 16:22:40 +0800

    selection done

--- app/src/main/java/com/example/finalproject/firstFragment.kt
+++ app/src/main/java/com/example/finalproject/firstFragment.kt
@@ -1,10 +1,12 @@
 package com.example.finalproject
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
