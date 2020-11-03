9da8509240f2ed1f9e10980f8612abea0d8ee5a7
Keith <limbokeith71@gmail.com>
2020-11-03 13:10:04 +0800

    finished fragment 2

--- app/src/main/java/com/example/finalproject/secondFragment.kt
+++ app/src/main/java/com/example/finalproject/secondFragment.kt
@@ -1,10 +1,13 @@
 package com.example.finalproject
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_second.view.*
 import kotlin.text.Typography.bullet
@@ -41,6 +44,38 @@ class secondFragment : Fragment() {
         val view: View = inflater!!.inflate(R.layout.fragment_second, container, false)
         val selectedText = args.selectedString
         view.selectTransac.text = selectedText
+
+        //Confirm
+        view.btnConfirm.setOnClickListener {
+            val builder = AlertDialog.Builder(activity)
+            builder.setMessage("Do you have your requirements now?")
+                .setCancelable(false)
+                .setPositiveButton("Yes") { dialog, id ->
+                    Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_thirdFragment)
+                }
+                .setNegativeButton("No") { dialog, id ->
+                    // Dismiss the dialog
+                    dialog.dismiss()
+                }
+            val alert = builder.create()
+            alert.show()
+        }
+
+        //Cancel
+        view.btnCancel.setOnClickListener {
+            val builder = AlertDialog.Builder(activity)
+            builder.setMessage("Do you want to go back?")
+                .setCancelable(false)
+                .setPositiveButton("Yes") { dialog, id ->
+                    Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment)
+                }
+                .setNegativeButton("No") { dialog, id ->
+                    // Dismiss the dialog
+                    dialog.dismiss()
+                }
+            val alert = builder.create()
+            alert.show()
+        }
         return view
     }
 
